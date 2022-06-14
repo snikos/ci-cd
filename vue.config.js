1fbd2966ccf438b14781d54b52c623caaf51980c
Niko <cobrascript@yandex.ru>
2022-06-14 19:09:55 +0300

    edit mongolia-comment

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,16 @@ module.exports = defineConfig({
         additionalData: "@import '@/assets/scss/app.scss';"
       },
     }
-  }
+  },
+  configureWebpack:{
+    performance: {
+      hints: false
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
+      }
+    }
+  },
 })
