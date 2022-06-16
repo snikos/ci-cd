44620c56a63fc0df3d44773de42f7b2e46a9ec0a
Niko <cobrascript@yandex.ru>
2022-06-16 14:17:49 +0300

    edit research and reload mongolia-comment

--- src/pages/MongoliaJson.vue
+++ src/pages/MongoliaJson.vue
@@ -1,34 +1,77 @@
 <template>
-    <div class="mongolia">
+    <div class="row mongolia">
         <h1>Mongolia</h1>
-        <div class="btn-toolbar pb-2">
-            <div class="btn-group-sm">
-                <button @click="searchHash" type="button" class="btn btn-warning">Search Hash</button>
-            </div>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <div class="input-group-text" id="btnGroupAddon">#</div>
+        <div class="col-12">
+            <div class="btn-toolbar pb-2">
+                <div class="input-group input-group-sm">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text" id="btnGroupAddon">#</div>
+                    </div>
+                    <input
+                            v-model="searchQuery"
+                            type="text"
+                            placeholder="Enter word..."
+                            aria-label="Input group example"
+                            aria-describedby="btnGroupAddon">
+                    <div class="input-group-append">
+                        <button
+                                type="button"
+                                class="btn btn-outline-secondary"
+                                @click="searchHashInput"
+                        >
+                            Go!
+                        </button>
+                    </div>
                 </div>
-                <input v-model="searchQuery" type="text" placeholder="Enter word..." aria-label="Input group example" aria-describedby="btnGroupAddon">
             </div>
         </div>
-        <ul class="row list-group">
-            <li
-                    class="col-12 list-group-item"
-                    v-for="({commentText, commentName, commentDate}, idx) in comments"
-                    :key="idx"
-            >
-                <div>
-                    <strong>{{ idx }}</strong> - {{ commentName }} ({{ commentDate }})
-                    <p>{{ commentText }}</p>
+        <div class="col-12 col-md-10">
+            <ul class="list-group">
+                <li
+                        class="list-group-item"
+                        v-for="({commentText, commentName, commentDate}, idx) in comments"
+                        :key="idx"
+                >
+                    <div>
+                        <strong>{{ idx }}</strong> - {{ commentName }} ({{ commentDate }})
+                        <p>{{ commentText }}</p>
+                    </div>
+                </li>
+                <li v-show="loading" class="loading">Loading...</li>
+            </ul>
+            <div class="btn-toolbar justify-content-sm-center mb-3">
+                <div class="btn-group btn-group-sm mt-3">
+                    <button
+                            @click="loadNextComments"
+                            type="button"
+                            class="btn btn-secondary"
+                            :disabled="showCommentsAreDone == 1"
+                    >Load {{ comCount }} comments</button>
+                    <button
+                            @click="loadAllComments"
+                            type="button"
+                            class="btn btn-secondary ml-3"
+                            :disabled="showCommentsAreDone == 1"
+                    >Load All comments</button>
                 </div>
-            </li>
-            <li v-show="loading" class="loading">Loading...</li>
-        </ul>
-        <div class="btn-toolbar justify-content-sm-center mb-3">
-            <div class="btn-group btn-group-sm mt-3">
-                <button @click="loadNextComments" type="button" class="btn btn-secondary">Load 10 comments</button>
-                <button @click="loadAllComments" type="button" class="btn btn-secondary ml-3">Load All comments</button>
+            </div>
+        </div>
+        <div class="col-12 col-md-2">
+            <div class="btn-toolbar pb-2">
+                <div class="btn-group-vertical btn-group-sm">
+                    <button
+                            @click="searchHashButton($event)"
+                            type="button"
+                            class="btn btn-warning"
+                            value="ольга"
+                    >ольга</button>
+                    <button
+                            @click="searchHashButton($event)"
+                            type="button"
+                            class="btn btn-warning"
+                            value="таджик"
+                    >таджик</button>
+                </div>
             </div>
         </div>
     </div>
@@ -45,7 +88,9 @@
                 comments: [],
                 cur: [],
                 loading: false,
+                showCommentsAreDone: 0,
                 nextCount: 0,
+                comCount: 100,
                 searchQuery: '',
             }
         },
@@ -59,7 +104,7 @@
                         const res = await mongoliaJson;
                         this.loadComments = [...res];
                         this.loadNextComments();
-                    }, 1000);
+                    }, 500);
                 }
                 catch (e) {
                     console.log('Error load: ' + e)
@@ -67,30 +112,44 @@
             },
             loadAllComments() {
                 this.comments = this.loadComments;
+                this.showCommentsAreDone = 1;
             },
             loadNextComments() {
                 this.loading = true;
                 setTimeout( async () => {
                     this.cur = await this.getFirstLoad;
-                    this.nextCount += 5;
+                    this.nextCount += this.comCount;
                     this.comments = [...this.comments, ...this.cur];
                     this.loading = false;
+                    if ( this.comments.length >= 800) {
+                        this.showCommentsAreDone = 1;
+                    }
                 }, 1000);
             },
-            searchHash() {
+            searchHashInput() {
                 let search = this.searchComments;
                 this.loading = true;
                 setTimeout( async () => {
                     this.comments = search;
                     this.loading = false;
-                }, 1000);
-                console.log(search);
+                }, 500);
+                console.log('search with input', search);
+            },
+            searchHashButton(event) {
+                this.searchQuery = event.target.value;
+                let search = this.searchComments;
+                this.loading = true;
+                setTimeout( async () => {
+                    this.comments = search;
+                    this.loading = false;
+                }, 500);
+                console.log('search with button: ', search);
             },
         },
         computed: {
             getFirstLoad() {
                 return [...this.loadComments].filter( (el, i) => {
-                    return ( i >= this.nextCount && i < this.nextCount+9 )
+                    return ( i >= this.nextCount && i < this.nextCount + this.comCount )
                 });
             },
             searchComments() {
