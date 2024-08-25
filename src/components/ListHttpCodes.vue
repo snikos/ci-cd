1b6f5138addb0ac6a904ba69d5ada6f30c91c92c
Niko <cobrascript@yandex.ru>
2024-08-25 10:42:23 +0800

    added: json, component and view Http Codes

---
+++ src/components/ListHttpCodes.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="table-responsive">
+    <ul>
+      <li v-for="(item, idx) in keyRun"
+        :key="item[idx]+'_'+idx">
+        <p>{{ item['title'] }}</p>
+        <table class="table table-sm table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>1xx</th>
+              <th>2xx</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(el, i) in item['arrCodes']"
+            :key="el['code']+'_'+i">
+              <td>{{ el['code'] }}</td>
+              <td>{{ el['status'] }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    name: "ListHttpCodes",
+    props: {
+      codes: {
+        type: Object,
+        required: true,
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+      keyRun() {
+        let x = Object.entries(this.codes).map(x => x[1]);
+        //console.log( x )
+        return x
+      }
+    },
+  }
+</script>
---
+++ src/views/HttpStatusCodes.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="raw">
+    <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+      <div class="list-group">
+        <ul class="pagination pagination-sm justify-content-center flex-wrap">
+          <li type="button" class="page-item">
+            <span class="page-link">1XX</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-12">
+      <ListHttpCodes :codes="loadCodes" />
+    </div>
+  </div>
+</template>
+
+<script>
+  import httpCodes from "@/json/http-status-codes.json";
+  import ListHttpCodes from "@/components/ListHttpCodes"
+  export default {
+    name: "HttpStatusCodes",
+    components: {
+      ListHttpCodes,
+    },
+    data(){
+      return {
+        loadCodes: [],
+      }
+    },
+    mounted(){
+      this.fetchCodes();
+    },
+    methods: {
+      async fetchCodes(){
+        try {
+          const res = await httpCodes;
+          this.loadCodes = res["codes"];
+        }
+        catch (e){
+          console.log("Error load fetch json http codes" + e)
+        }
+      },
+    },
+    computed: {},
+  }
+</script>
