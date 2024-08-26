e0ac896aa633e31b11b280baa33d807cae24756c
Niko <cobrascript@yandex.ru>
2024-08-26 23:16:24 +0800

    Yes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -18,6 +19,14 @@ module.exports = defineConfig({
         minSize: 10000,
         maxSize: 250000,
       }
-    }
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        //'__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false),
+        // Replace with true if detailed mismatch info is needed
+        //'__VUE_OPTIONS_API__': JSON.stringify(true),
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+      })
+    ],
   },
-})
+});
