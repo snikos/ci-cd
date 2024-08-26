75f169f540329db71f86be7237d1260d76cf3e1d
Niko <cobrascript@yandex.ru>
2024-08-26 22:06:19 +0800

    Boom

--- src/hooks/testRef.js
+++ src/hooks/testRef.js
@@ -2,7 +2,7 @@ import {ref, computed, onMounted, onUpdated, onUnmounted} from "vue";
 
 export function useTestRef(x) {
     const testRef = ref(x);
-    const doubleTestRef = computed(() => testRef.value += 'Does that mean the Composition API is superior in every way to Vue’s classic API? No. In most cases you’ll be fine to stick with the classic API. But if you’re planning to reuse code, the Composition API is unquestionably superior.');
+    const doubleTestRef = computed(() => testRef.value = 'Does that mean the Composition API is superior in every way to Vue’s classic API? No. In most cases you’ll be fine to stick with the classic API. But if you’re planning to reuse code, the Composition API is unquestionably superior.');
     function incrementTestRef(value) {
         testRef.value += value;
     }
