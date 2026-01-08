aceaf6879530f92b0c16bc2b4ada9268cd39296a
Niko <cobrascript@yandex.ru>
2026-01-08 10:33:23 +0800

    Edit Nvm

---
+++ src/composable/fetch.js
@@ -0,0 +1,25 @@
+import { ref, watchEffect } from 'vue';
+
+export function useFetch(url) {
+  const data  = ref(null);
+  const error = ref(null);
+
+  const fetchData = () => {
+    data.value  = null;
+    error.value = null;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => data.value = json)
+      .catch((err) => error.value = err);
+  }
+
+  watchEffect(() => {
+    fetchData();
+  });
+
+  return {
+    data,
+    error,
+  }
+}
--- src/pages/Nvm.vue
+++ src/pages/Nvm.vue
@@ -24,83 +24,60 @@
     </div>
   </div>
   <div class="row">
-    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
-      <div class="list-group">
-        <div class="list-group-item text-left">
-          <span>установка</span> -
-          <code>nvm install 8.9.4</code>
-        </div>
-        <div class="list-group-item text-left">
-          <span>последняя Нода</span> -
-          <code>nvm install latest</code>
-        </div>
-        <div class="list-group-item text-left">
-          <span>удаление Ноды</span> -
-          <code>nvm uninstall 0.11</code>
-        </div>
-        <div class="list-group-item text-left">
-          <span>последний патч</span> -
-          <code>nvm install 8.9</code>
-        </div>
-        <div class="list-group-item text-left">
-          <span>Установка Ноды 9.0.0 и переустановка npm 8.9</span> -
-          <code>nvm install v9.0.0 --reinstall-packages-from=8.9</code>
+    <template v-for="(item, idx) in formatRules" 
+      :key="Math.random + '_' + idx">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+        <div class="list-group">
+          <div v-for="({ text, comd }, i) in item" 
+            class="list-group-item text-left fx-bg" 
+            :key="Math.random + '_' + i">
+            <p class="mb-1">{{ text }}</p>
+            <kbd>{{comd}}</kbd>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
-      <div class="list-group">
-        <div class="list-group-item text-left">
-          <span>список</span> -
-          <code>nvm list</code>
-        </div>
-        <div class="list-group-item text-left">
-          <span>список доступных версий</span> -
-          <code>nvm list available</code>
-        </div>
-        <div class="list-group-item text-left">
-          <span>список скаченных версий</span> -
-          <code>nvm list installed</code>
-        </div>
-        <div class="list-group-item text-left">
-          <span>node</span> -
-          <code>node -v</code>
-        </div>
-      </div>
-        <div class="list-group-item text-left">
-          <span>npm</span> -
-          <code>npm -v</code>
-        </div>
-    </div>
-    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
-      <div class="list-group">
-        <div class="list-group-item text-left">
-          <span>текущая версия комманды</span> -
-          <code>nvm current</code>
-        </div>
-        <div class="list-group-item text-left">
-          <span>Использование NVM и список версий</span> -
-          <code>nvm use node nvm ls</code>
-        </div>
-        <div class="list-group-item text-left">
-          <span>список Нод установленных в системе</span> -
-          <code>nvm ls</code>
-        </div>
-        <div class="list-group-item text-left">
-          <span>перейти на Node.js версии 8.9.4</span> -
-          <code>nvm use 8.9.4</code>
-        </div>
-        <div class="list-group-item text-left">
-          <span>перейти на последнюю версию Node.js</span> -
-          <code>nvm use node</code>
-        </div>
-      </div>
-    </div>
+    </template>
   </div>
 </template>
 
 <script>
+  //import { ref } from 'vue';
+  import nvmRules from '@/json/nvm.json';
   export default {
     name: 'NvmWindows',
+    myJson: nvmRules,
+    methods: {
+      chunkArray(array, size) {
+        const result = [];
+        for (let i = 0; i < array.length; i += size) {
+          result.push(array.slice(i, i + size));
+        }
+        return result;
+      }
+    },
+    computed: {
+      getRules() {
+        return this.$options.myJson["nvm"]
+      },
+      formatRules() {
+        return this.chunkArray(this.getRules, 3)
+      }
+    },
+    setup() {
+
+      return {
+        
+      }
+    }
   }
 </script>
+
+<style scoped>
+  .fx-bg {
+    background-color: #fcfcfc;
+    color: black;
+    border-left: 1px dashed #ccc;
+    background: repeating-linear-gradient(to bottom, transparent, transparent 10px, #f4f4f4 10px, #f4f4f4 11px), repeating-linear-gradient(to right, transparent, transparent 10px, #f4f4f4 10px, #f4f4f4 11px);
+    background-clip: padding-box;
+  }
+</style>
